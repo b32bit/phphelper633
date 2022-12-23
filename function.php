be0855679f82c1613ab3c09a238de3e947d358e0
mon3 maz <108606471+b32bit@users.noreply.github.com>
2022-12-23 16:44:23 +0700

    Create function.php

---
+++ function.php
@@ -0,0 +1,18 @@
+function console_log($args = false){
+	$args_as_json = json_encode($args);
+
+	$js_code = "<script>console.log('%c 💬 PHP: ','background: #474A8A; color: #B0B3D6; line-height: 2',";
+	$js_code .= "{$args_as_json},";
+	$js_code .= ")</script>";
+
+	echo $js_code;
+}
+
+function dd($var=false){
+	if($var!=false){
+
+		echo '<blockquote style="padding:22px;background:lightyellow" >';
+		nl2br(print_r($var));
+		echo '</blockquote>';
+	}
+}
